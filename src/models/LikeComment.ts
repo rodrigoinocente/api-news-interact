2f105ebb4dac10798bb6606be4d643e4c53a97c0
Rodrigo Inocente <ridrigocorreia@gmail.com>
2024-11-18 17:41:35 -0300

    feat: Add hooks to LikeCommentSchema for syncing likeCount with CommentModel after save and update operations, ensuring consistent like count updates.

--- src/models/LikeComment.ts
+++ src/models/LikeComment.ts
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import { CommentModel, LikeCommentModel } from "../database/db";
+import { ILikeComment } from "../../custom";
 
 const LikeCommentSchema = new mongoose.Schema({
     dataCommentId: {
@@ -26,6 +28,20 @@ const LikeCommentSchema = new mongoose.Schema({
             _id: false
         }]
     },
+});
+
+LikeCommentSchema.post('save', async function () {
+    await CommentModel.updateOne({ _id: this.dataCommentId, "comment._id": this.commentId },
+        { $set: { "comment.$.likeCount": this.likes.length } });
+});
+
+LikeCommentSchema.post('findOneAndUpdate', async function () {
+    const dataLikesId = this.getQuery();
+    const dataLikesUpdate: ILikeComment | null = await LikeCommentModel.findById(dataLikesId);
+    if (dataLikesUpdate) {
+        await CommentModel.updateOne({ _id: dataLikesUpdate.dataCommentId, "comment._id": dataLikesUpdate.commentId },
+            { $set: { "comment.$.likeCount": dataLikesUpdate.likes.length } });
+    }
 });
 
 export default LikeCommentSchema;
